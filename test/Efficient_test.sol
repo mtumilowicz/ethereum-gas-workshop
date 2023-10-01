7cd0d9323bb9b6253fcef2524f07bd644324edbe
mtumilowicz <michal.tumilowicz01@gmail.com>
2023-10-01 10:00:42 +0200

    tests

--- test/Efficient_test.sol
+++ test/Efficient_test.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 
 import "remix_tests.sol";
 import "../contracts/Efficient.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract EfficientContractTest {
 
@@ -11,88 +12,130 @@ contract EfficientContractTest {
     }
 
     function t1 () public {
+        // given
         string[] memory data1 = new string[](3);
         data1[0] = "a";
         data1[1] = "b";
         data1[2] = "c";
+
+        // when
         contractInstance.addAll(data1);
-        Assert.equal(contractInstance.getLength(), 3, "length should be 3");
-        Assert.equal(contractInstance.count("a"), 1, "a appears 1");
-        Assert.equal(contractInstance.count("b"), 1, "b appears 1");
-        Assert.equal(contractInstance.count("c"), 1, "c appears 1");
+
+        // then
+        lengthCheck(3);
+        countCheck("a", 1);
+        countCheck("b", 1);
+        countCheck("c", 1);
     }
 
     function t11 () public {
+        // given
         string[] memory data1 = new string[](3);
         data1[0] = "a";
         data1[1] = "a";
         data1[2] = "a";
+
+        // when
         contractInstance.addAll(data1);
-        Assert.equal(contractInstance.getLength(), 3, "length should be 3");
-        Assert.equal(contractInstance.count("a"), 3, "a appears 3");
+
+        // then
+        lengthCheck(3);
+        countCheck("a", 3);
     }
 
     function t2() public {
-        // Test Case 2
+        // given
         string[] memory data2 = new string[](4);
         data2[0] = "a";
         data2[1] = "a";
         data2[2] = "b";
         data2[3] = "b";
+
+        // when
         contractInstance.addAll(data2);
-        Assert.equal(contractInstance.getLength(), 4, "length should be 4");
-        Assert.equal(contractInstance.count("a"), 2, "a appears 2");
-        Assert.equal(contractInstance.count("b"), 2, "b appears 2");
+
+        // then
+        lengthCheck(4);
+        countCheck("a", 2);
+        countCheck("b", 2);
     }
 
     function t3() public {
+        // given
         string[] memory data3 = new string[](5);
         data3[0] = "a";
         data3[1] = "b";
         data3[2] = "a";
         data3[3] = "c";
         data3[4] = "b";
+
+        // when
         contractInstance.addAll(data3);
-        Assert.equal(contractInstance.getLength(), 5, "length should be 5");
-        Assert.equal(contractInstance.count("a"), 2, "a appears 2");
-        Assert.equal(contractInstance.count("b"), 2, "b appears 2");
-        Assert.equal(contractInstance.count("c"), 1, "c appears 1");
+
+        // then
+        lengthCheck(5);
+        countCheck("a", 2);
+        countCheck("b", 2);
+        countCheck("c", 1);
     }
 
     function t33() public {
+        // given
         string[] memory data3 = new string[](5);
         data3[0] = "a";
         data3[1] = "b";
         data3[2] = "c";
         data3[3] = "d";
         data3[4] = "d";
+
+        // when
         contractInstance.addAll(data3);
-        Assert.equal(contractInstance.getLength(), 5, "length should be 5");
-        Assert.equal(contractInstance.count("a"), 1, "a appears 1");
-        Assert.equal(contractInstance.count("b"), 1, "b appears 1");
-        Assert.equal(contractInstance.count("c"), 1, "c appears 1");
-        Assert.equal(contractInstance.count("d"), 2, "d appears 2");
+
+        // then
+        lengthCheck(5);
+        countCheck("a", 1);
+        countCheck("b", 1);
+        countCheck("c", 1);
+        countCheck("d", 2);
     }
 
     function t333() public {
+        // given
         string[] memory data3 = new string[](5);
         data3[0] = "d";
         data3[1] = "a";
         data3[2] = "b";
         data3[3] = "c";
         data3[4] = "d";
+
+        // when
         contractInstance.addAll(data3);
-        Assert.equal(contractInstance.getLength(), 5, "length should be 5");
-        Assert.equal(contractInstance.count("a"), 1, "a appears 1");
-        Assert.equal(contractInstance.count("b"), 1, "b appears 1");
-        Assert.equal(contractInstance.count("c"), 1, "c appears 1");
-        Assert.equal(contractInstance.count("d"), 2, "d appears 2");
+
+        // then
+        lengthCheck(5);
+        countCheck("a", 1);
+        countCheck("b", 1);
+        countCheck("c", 1);
+        countCheck("d", 2);
     }
 
     function t4() public {
+        // given
         string[] memory data4 = new string[](0);
+
+        // when
         contractInstance.addAll(data4);
-        Assert.equal(contractInstance.getLength(), 0, "length should be 0");
-        Assert.equal(contractInstance.count("a"), 0, "a appears 0");
+
+        // then
+        lengthCheck(0);
+        countCheck("a", 0);
+    }
+
+    function lengthCheck(uint expectedValue) private {
+        Assert.equal(contractInstance.getLength(), expectedValue, string.concat("length should be ", Strings.toString(expectedValue)));
+    }
+
+    function countCheck(string memory searchedString, uint expectedCount) private {
+        Assert.equal(contractInstance.count(searchedString), expectedCount,  string.concat(searchedString, " should appear ", Strings.toString(expectedCount)));
     }
 }
